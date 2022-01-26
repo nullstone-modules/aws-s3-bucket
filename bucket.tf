e23daa9f597afd7a7ed2ab365ecf19dd4139fb2d
Scott Sickles <scottsickles@gmail.com>
2022-01-26 11:46:47 -0500

    s3 bucket is being created successfully

--- bucket.tf
+++ bucket.tf
@@ -4,61 +4,15 @@ resource "aws_s3_bucket" "this" {
   tags          = local.tags
   force_destroy = true
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.s3_policy.json
-}
-
-data "aws_iam_policy_document" "s3_policy" {
-  // Don't add the policy statement if we don't have any origin access identities
-  // The IAM policy would be invalid with no principal identifiers
-  dynamic "statement" {
-    for_each = length(local.oai_iam_arns) > 0 ? [local.oai_iam_arns] : []
+  dynamic "server_side_encryption_configuration" {
+    for_each = var.server_side_encryption ? ["fake"] : []
 
     content {
-      sid       = "AllowOriginReadObject"
-      actions   = ["s3:GetObject"]
-      resources = ["arn:aws:s3:::${local.resource_name}/*"]
-
-      principals {
-        type        = "AWS"
-        identifiers = statement.value
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "aws:kms"
+        }
       }
-    }
-  }
-
-  statement {
-    sid       = "AllowReadBucket"
-    actions   = ["s3:ListBucket"]
-    resources = ["arn:aws:s3:::${local.resource_name}"]
-
-    principals {
-      type        = "AWS"
-      identifiers = concat([aws_iam_user.deployer.arn], local.oai_iam_arns)
-    }
-  }
-
-  statement {
-    sid = "AllowDeployerWrite"
-    actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:DeleteObject"
-    ]
-    resources = ["arn:aws:s3:::${local.resource_name}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_user.deployer.arn]
     }
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,6 @@
 output "region" {
-  value = data.aws_region.this.name
+  value       = data.aws_region.this.name
+  description = "string ||| The AWS region that the S3 bucket will be created in."
 }
 
 output "bucket_arn" {
@@ -10,16 +11,4 @@ output "bucket_arn" {
 output "bucket_name" {
   value       = aws_s3_bucket.this.bucket
   description = "string ||| The name of the created S3 bucket."
-}
-
-output "deployer" {
-  value = {
-    name       = aws_iam_user.deployer.name
-    access_key = aws_iam_access_key.deployer.id
-    secret_key = aws_iam_access_key.deployer.secret
-  }
-
-  description = "object({ name: string, access_key: string, secret_key: string }) ||| An AWS User with explicit privilege to deploy to the S3 bucket."
-
-  sensitive = true
 }
