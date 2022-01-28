5e0ca82802533558c4f4462e9c879a14af6f4724
Scott Sickles <scottsickles@gmail.com>
2022-01-28 11:34:27 -0500

    updated the outputs of the s3 bucket

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,19 @@
-output "region" {
-  value       = data.aws_region.this.name
-  description = "string ||| The AWS region that the S3 bucket will be created in."
-}
-
-output "bucket_arn" {
+output "db_arn" {
   value       = aws_s3_bucket.this.arn
   description = "string ||| The ARN of the created S3 bucket."
 }
 
-output "bucket_name" {
+output "db_protocol" {
+  value       = "s3"
+  description = "string ||| The protocol used to connect to the s3 bucket."
+}
+
+output "db_hostname" {
   value       = aws_s3_bucket.this.bucket
   description = "string ||| The name of the created S3 bucket."
+}
+
+output "db_port" {
+  value       = ""
+  description = "string ||| The port for s3 buckets is blank."
 }
