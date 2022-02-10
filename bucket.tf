dcb36328a7ece3c49cc16d0a4deccb2a126cf0a0
Brad Sickles <brad.sickles@gmail.com>
2022-02-10 14:04:37 -0500

    Upgrading aws tf provider

--- bucket.tf
+++ bucket.tf
@@ -1,22 +1,30 @@
 resource "aws_s3_bucket" "this" {
   bucket        = local.resource_name
-  acl           = "private"
   tags          = local.tags
   force_destroy = true
+}
 
-  versioning {
-    enabled = var.versioning
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.versioning ? "Enabled" : "Disabled"
   }
+}
 
-  dynamic "server_side_encryption_configuration" {
-    for_each = var.server_side_encryption ? ["fake"] : []
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
 
-    content {
-      rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "aws:kms"
-        }
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
     }
   }
+
+  count = var.server_side_encryption ? 1 : 0
 }
