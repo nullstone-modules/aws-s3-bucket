6415c3dad75f423936a62fd167bd18d01dce565f
Scott Sickles <scottsickles@gmail.com>
2022-02-15 20:32:31 -0500

    fix value typo

--- bucket.tf
+++ bucket.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
 
   versioning_configuration {
-    status = var.versioning ? "Enabled" : "Disabled"
+    status = var.versioning ? "Enabled" : "Suspended"
   }
 }
 
