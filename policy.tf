e6d9f4a50e2894f9485b6ceddf100a4bc0a817ee
Scott Sickles <scottsickles@gmail.com>
2023-11-09 11:22:48 -0500

    add a policy when we need to make the bucket read only

---
+++ policy.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket_policy" "public_read_only" {
+  count = var.public_read_only ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.public_read_only.json
+}
+
+data "aws_iam_policy_document" "public_read_only" {
+  statement {
+    sid     = "PublicReadGetObject"
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+    resources = ["${aws_s3_bucket.this.bucket}/*"]
+  }
+}
