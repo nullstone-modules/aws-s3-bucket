f0a8d5915ba26c9441597d3bfb63561b31e596d7
Scott Sickles <scottsickles@gmail.com>
2023-11-09 11:35:47 -0500

    squash merge f/1753/public-read-only

--- variables.tf
+++ variables.tf
@@ -13,6 +13,7 @@ variable "server_side_encryption" {
   description = <<EOF
 Encrypts all data at the object level as it is being written to S3, automatically decrypts it as you access it.
 The way you access objects (presigned URLs, listing objects, and retrieving objects) all works the same way as if they were not encrypted.
+Public urls that are not presigned will not load because the file can't be decrypted.
 EOF
 }
 
@@ -22,5 +23,7 @@ variable "public_read_only" {
   description = <<EOF
 If toggled on, the contents of this S3 bucket will be made publicly accessible. Public access will be read-only.
 The ability to add, remove, or update via authenticated access will remain unchanged.
+If you make this bucket public, be sure to turn off server_side_encryption unless you are generating presigned URLs.
+When accessing a file via a public url, the file will not be able to be decrypted.
 EOF
 }
